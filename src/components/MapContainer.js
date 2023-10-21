f3e3e536e147fd355b47a05fa0e74dd7d7c5cb16
RyukiKuwahara <ryukiKuwahara@outlook.jp>
2023-10-22 07:42:49 +0900

    #2 markerFでマーカーを配置できるようにした

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF} from '@react-google-maps/api';
 const MapContainer = () => {
   
   const mapStyles = {        
@@ -10,6 +10,25 @@ const MapContainer = () => {
     lat: 35.4123, lng: 139.4132
   }
 
+  const locations = [
+    {
+      name: "Location 1",
+      location: { 
+        lat: 35.4123,
+        lng: 139.4132 
+      },
+      
+    },
+    {
+      name: "Location 2",
+      location: { 
+        lat: 35.4134,
+        lng: 139.4143 
+      },
+      
+    }
+  ];
+
   const apiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
   
   return (
@@ -17,9 +36,17 @@ const MapContainer = () => {
        googleMapsApiKey={apiKey}>
         <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={13}
+          zoom={18}
           center={defaultCenter}
-        />
+        >
+          {
+              locations.map(item => {
+                return (
+                <MarkerF key={item.name} position={item.location}/>
+                )
+              })
+          }
+         </GoogleMap>
      </LoadScript>
   )
 }
