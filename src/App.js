1e337b9c34f6ea8cec29828eff282d85fb52e03e
RyukiKuwahara <ryukiKuwahara@outlook.jp>
2023-10-21 20:30:51 +0900

    -

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Bio-Map-frontend" element={<Home />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main-service" element={<MainService />} />
