2e47c070635d978f8c92568a7ce5aa6b77544138
RyukiKuwahara <ryukiKuwahara@outlook.jp>
2023-10-29 19:18:58 +0900

    バッジ，ポストがない場合のメッセージを追加

--- src/components/BadgeGallery.js
+++ src/components/BadgeGallery.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './BadgeGallery.css'
 
 function BadgeGallery({ badges }) {
-  return (
-    <div className="badge-gallery">
-      {badges.map((badge, index) => (
-        <div key={index} className="badge-item">
-          <img src={`data:image/jpg;base64,${badge.image_data}`} alt="" />
-        </div>
-      ))}
-    </div>
-  );
-}
+    return (
+      <div className="badge-gallery">
+        {badges.length > 0 ? (
+          badges.map((badge, index) => (
+            <div key={index} className="badge-item">
+              <img src={`data:image/jpg;base64,${badge.image_data}`} alt="" />
+            </div>
+          ))
+        ) : (
+          <p>バッジはありません。</p>
+        )}
+      </div>
+    );
+  }
+  
 
 export default BadgeGallery;
--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -4,16 +4,21 @@ import './ImageGallery.css';
 function ImageGallery({ posts }) {
   return (
     <div className="image-gallery">
-      {posts.map((post, index) => (
-        <div key={index} className="image-item">
-          <h2>{post.name}</h2>
-          <p>緯度：{post.lat}, 経度：{post.lng}</p>
-          <img src={`data:image/jpg;base64,${post.image_data}`} alt="" />
-          <p>{post.explain}</p>
-        </div>
-      ))}
+      {posts.length > 0 ? (
+        posts.map((post, index) => (
+          <div key={index} className="image-item">
+            <h2>{post.name}</h2>
+            <p>緯度：{post.lat}, 経度：{post.lng}</p>
+            <img src={`data:image/jpg;base64,${post.image_data}`} alt="" />
+            <p>{post.explain}</p>
+          </div>
+        ))
+      ) : (
+        <p>ポストがありません。</p>
+      )}
     </div>
   );
 }
+
 
 export default ImageGallery;
