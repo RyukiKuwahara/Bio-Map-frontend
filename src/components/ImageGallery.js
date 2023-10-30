4cf6ae9f266c474c37f79463d82cda8b1ca940a0
RyukiKuwahara <ryukiKuwahara@outlook.jp>
2023-10-31 02:08:44 +0900

    ImageGalleryの構造を変更

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -3,20 +3,22 @@ import './ImageGallery.css';
 
 function ImageGallery({ posts }) {
   return (
-    <div className="image-gallery">
-      {posts.length > 0 ? (
-        posts.map((post, index) => (
+    <>
+    {posts.length > 0 ? (
+      posts.map((post, index) => (
+        <div className="image-gallery">
           <div key={index} className="image-item">
             <h2>{post.name}</h2>
             <p>緯度：{post.lat}, 経度：{post.lng}</p>
             <img src={`data:image/jpg;base64,${post.image_data}`} alt="" />
             <p>{post.explain}</p>
           </div>
+        </div>
         ))
       ) : (
-        <p>ポストがありません。</p>
+        <p className='none-post'>ポストがありません。</p>
       )}
-    </div>
+    </>
   );
 }
 
